98dee31a8ed08b70b2fa75e63c219aa0786bb681
Abubakar Al-Hussein <al-hussein.abubakar@amalitech.org>
2021-11-11 11:32:45 +0000

    Update menu-items to navigate with router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
 import HomePage  from './pages/homepage/homepage.component';
+import {Route, Routes } from 'react-router-dom';
 
-function App() {
+const HatsPage = ( props) => {
+  console.log(props);
   return (
     <div>
-       <HomePage/> 
+      <h1>HATS PAGE</h1>  
+    </div>
+  );
+  
+}
+function App() {
+  return (  
+    <div>
+      
+      <Routes>
+        <Route  path='/' element={<HomePage />} />
+        <Route  path='/hats' element={<HatsPage />} />
+      </Routes>
+    
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,13 +1,16 @@
- import React from 'react';
+import React from 'react';
+// import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
-    <div 
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}>
+    <div
       className='background-image'
       style={{
-        backgroundImage: `url(${imageUrl})`
+        backgroundImage: `url(${imageUrl})`,
       }}
     />
     <div className='content'>
